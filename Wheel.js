366bcd8a050bff88d6f5b86492899d78fc976381
Paola Guerrero <pguerrero.sh@mysummitps.otg>
2016-05-31 20:39:39 +0000

    update

---
+++ Wheel.js
@@ -0,0 +1,10 @@
+var crust=document.getElementById("base");
+var sauce=document.getElementById("glue");
+var cheese=document.getElementById("glue2");
+var toppings=document.getElementById("cherry");
+
+crust.addEventListener("click");
+sauce.addEventListener("click");
+cheese.addEventListener("click");
+toppings.addEventListener("click");
+
